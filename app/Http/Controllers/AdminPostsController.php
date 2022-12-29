af8c0a66fc91e811c09c1227000f53cf4b7dc9ae
ArTim <3a932118@gm.student.ncut.edu.tw>
2022-12-29 14:41:04 +0800

    AdminPostsController 的 store()、 update() 加入資料驗證規則

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -21,6 +21,11 @@ class AdminPostsController extends Controller
 
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'title' => 'required|min:3|max:255',
+            'content' => 'required',
+            'is_feature' => 'required|Boolean',
+        ]);
         Post::create($request->all());
         // 將 View 送出的表單資料跟 Post 這個 Model 連接(Model 用來存取資料表)，進行新增
         return redirect()->route('admin.posts.index');
@@ -34,6 +39,11 @@ class AdminPostsController extends Controller
 
     public function update(Request $request, Post $post)
     {
+        $this->validate($request, [
+            'title' => 'required|min:3|max:255',
+            'content' => 'required',
+            'is_feature' => 'required|Boolean',
+        ]);
         $post->update($request->all());
         return redirect()->route('admin.posts.index');
     }
